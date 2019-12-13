de1b1af5e6b3e23c5a90e4da37d3abc3082f0572
cobellisantiago <36997907+cobellisantiago@users.noreply.github.com>
2019-12-13 12:27:01 -0300

    Agregado metodo generarPremio + generarDescuentos

--- src/main/java/com/cobelliluetichperezvazquez/trabajointegrador/gestores/GestorPremio.java
+++ src/main/java/com/cobelliluetichperezvazquez/trabajointegrador/gestores/GestorPremio.java
@@ -9,5 +9,38 @@ public class GestorPremio {
     @Autowired
     private GestorBaseDeDatos gestorBaseDeDatos;
 
+public Premio generarPremio(Integer sumaAsegurada){
+        Premio premio = new Premio();
+        double totalPremio = (sumaAsegurada*0.05);
+        double prima = (sumaAsegurada*0.01);
+        double derechoEmision = (double) (Math.random()*100);
+        premio.setTotalPremio((float) totalPremio);
+        premio.setPrima((float) prima);
+        premio.setDerechoDeEmision((float) derechoEmision);
+        try {
+            gestorBaseDeDatos.savePremio(premio);
+        }
+        catch (Exception e) {
+            throw new NullPointerException("El hijo no pudo ser guardado en la BDD");
+        }
+        return premio;
+    }
 
+    public Descuentos generarDescuentos(Integer sumaAsegurada, FormaDePago formaDePago){
+        Descuentos descuentos = new Descuentos();
+        double descuentoPorUnidad = (sumaAsegurada*0.1);
+        double descuentoPorPagoSemestral = 0;
+        if(formaDePago.equals(formaDePago.SEMESTRAL))
+            descuentoPorPagoSemestral = sumaAsegurada*0.2;
+        descuentos.setDescuentoPorPagoSemestral((float) descuentoPorPagoSemestral);
+        descuentos.setDescuentoPorUnidad((float) descuentoPorUnidad);
+        try {
+            gestorBaseDeDatos.saveDescuentos(descuentos);
+        }
+        catch (Exception e) {
+            throw new NullPointerException("El hijo no pudo ser guardado en la BDD");
+        }
+        return descuentos;
+    }
+    
 }
